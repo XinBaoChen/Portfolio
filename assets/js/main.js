a3c27feeeb99d86df2ee8b2008db8f26ff87580d
XinBao Chen <66971849+XinBaoChen@users.noreply.github.com>
2023-09-05 21:43:05 -0400

    Finish portfolio :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,15 +39,102 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+contactMesseage = document.getElementById("contact-message")
 
+const sendEmail = (e)=>{
+    e.preventDefault()
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm("service_iywgmvm","template_8862uaw","#contact-form","54Khzb2e5Gn0ic3N5")
+    .then(() =>{
+        //show message sent
+        contactMesseage.textContent = "Message sent successfully ✅"
+
+        //remove messeage after five seconds
+        setTimeout(() =>{
+            contactMesseage.textContent = ""
+        },5000)
+    //clear input fields
+    contactForm.reset()
+}, () =>{
+    // Show error message
+    contactMesseage.textContent = "Message not sent (service error) ❌"
+})
+}
+
+contactForm.addEventListener("submit",sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = "ri-sun-fill"
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-fill'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+// const str = ScrollReveal({
+//     origin: "top",
+//     distance: "60px",
+//     duration: 2500,
+//     delay: 400,
+//     reset: true,
+//     //reset: true //Animation repeat
+
+// })
+
+// sr.reveal(`.home__perfil, .about__image`, {origin: "right"})
+// sr.reveal(`.home__name, .home__info,
+// .about__container .section__title-1, .about__info `, {origin: "rleft"})
